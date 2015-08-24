f4ae78436ad4dd34d30a42dbc4d5a4ad21a740ef
Kenneth Liu & Matt Bogen <pair+ksliu25.zebogen@devbootcamp.com>
2015-08-24 16:50:11 -0500

    Add Procedure model and migration, update logs migration

--- db/migrate/20150824194250_create_logs.rb
+++ db/migrate/20150824194250_create_logs.rb
@@ -1,10 +1,9 @@
 class CreateLogs < ActiveRecord::Migration
   def change
     create_table :logs do |t|
-      t.text :procedure, null: false
       t.text :observation, null: false
       t.integer :logger_id, null: false
-      t.integer :experiment_id, null: false
+      t.integer :procedure_id, null: false
 
       t.timestamps null: false
     end
