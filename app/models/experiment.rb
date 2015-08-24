a262f87bb6c8467a42f0b532e9c27940da36a025
Kenneth Liu & Matt Bogen <pair+ksliu25.zebogen@devbootcamp.com>
2015-08-24 15:12:24 -0500

    Add validations and associations to Experiment model

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -1,2 +1,9 @@
 class Experiment < ActiveRecord::Base
+  belongs_to :owner, class_name: "User"
+  belongs_to :lab
+  has_many :logs
+  has_many :assignments
+  has_many :staffs, through: :assignments
+  validates_presence_of :status, :title, :hypothesis, :description, :owner_id, :lab_id
+  validates :status, in: { inclusion: ["proposed", "in progress", "complete"] }
 end
