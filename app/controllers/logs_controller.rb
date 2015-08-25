c136f8bf7a86992f0ab8a7be476f4a7171bc7bc4
Kenneth Liu <ksliu25@gmail.com>
2015-08-24 22:55:52 -0500

    Add to logs controller

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,5 +1,5 @@
 class LogsController < ActionController::Base
-	# Logs per Experiment, contained in experiment controller?
+	# Logs per Experiment, contained in experiment controller? For now, have them exclusive of each other
 	# def index
 	# end
 
@@ -9,21 +9,36 @@ class LogsController < ActionController::Base
 
 	def create
 		@log = Log.create(log_params)
+		if @log.valid?
+			redirect_to @log
+		else
+			render :new
+		end
 	end
 
 	def show
+		@log = find_log
 	end
 
 	def update
+		@log = find_log
+		@log.update(log_params)
+		redirect_to @log
 	end
 
 	def delete
+		Log.destroy(param[:id])
+		redirect_to logs_path
 	end
 
 	private
 
+		def find_log
+			Log.find(params[:id])
+		end
+
 		def log_params
-			
+			params.require(:log).permit(:observation, :logger_id, :procedure_id)
 		end
 
 
