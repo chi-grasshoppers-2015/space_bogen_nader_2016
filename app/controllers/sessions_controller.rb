dd6706ed3436caaff77318fa2809d50ded8ceace
Connor Ring & Nicholas Kincaid <pair+ccring.nbkincaid@devbootcamp.com>
2015-08-25 09:31:51 -0500

    add sessions controller and route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(params[:user])
+    if User.authenticate(@user.email, @user.password)
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      render "new"
+    end
+  end
+
+  def delete
+    session.clear
+    redirect_to :root
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :delete]
   resources :labs, only: [:index, :show]
   resources :experiments
   resources :procedures, except: [:show, :index]
