a314cbd0e759c48852ac19d471c8b0ca52fe8ccf
Connor Ring & Kenneth Liu <pair+ccring.ksliu25@devbootcamp.com>
2015-08-26 15:45:26 -0500

    add lab seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Lab.create(name: "General Lab", location: "Matt Baker's Basement", allotted_time: 400)
+Lab.create(name: "Meth Lab", location: "Connor's House", allotted_time: 300)
+Lab.create(name: "Poop Lab", location: "Bogen's House", allotted_time: 150)
+Lab.create(name: "Redundancy Lab of Redundancy", location: "Redundant st Street", allotted_time: 300)
